a25705859d26d789ebaa542e2f912e5672690692
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:18:54 +0300

    Add files via upload

---
+++ work_15.py
@@ -0,0 +1,32 @@
+#Boolean 35
+# x,y,x1,y1=map(input().split())
+# t=(x+y)%2==(x1+y1)%2
+# print(t)
+#Boolean 36
+# x1,x2,y1,y2=map(int, input().split())
+# rux = (x1==x2 or y1==y2)
+# print(rux)
+#Boolean 37
+# x1,x2,y1,y2=map(int, input().split())
+# t1=x1+1==x2 and y2==y1+1
+# t2=y1==y2 and x1+1==x2
+# t3=y1==y2 and x1-1==x2
+# t4=x1==x2 and y1-1==y2
+# t5=x1==x2 and y1+1==y2
+# t6=x1+1==x2 and y2==y1-1
+# t7=x1-1==x2 and y2==y1+1
+# t8=x1-1==x2 and y2==y1-1
+# print(t1 or t2 or t3 or t4 or t5 or t6 or t7 or t8)
+#Boolean 38
+# x1, x2, y1, y2 = map(int, input().split())
+# fil=abs(x2-x1)==abs(y2-y1)
+# print(fil)
+#Boolean 39
+# x1,x2,y1,y2=map(int,input().split())
+# farzin=abs(x2-x1)==abs(y2-y1) and (x2==x1 or y2==y1)
+# print(farzin)
+# Boolean 40
+# x1,x2,y1,y2=map(int,input().split())
+# t1=abs(x2-x1==1) and abs(y2-y1==2)
+# t2=abs(x2-x1==2) and abs(y2-y1==1)
+# print(t1 or t2)
